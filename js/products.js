c883f06e693d97f39186059449e78d2069234d66
ievabi <ibidermane@gmail.com>
2021-05-29 20:27:36 +0200

    product_accessories1_colorpicker changed image sizes, moved all unused files to a new folder notUsedFiles

--- js/products.js
+++ js/products.js
@@ -1,10 +1,10 @@
 // this works, do not change
 $('#btn-black').on('click', function() {
-    $(".fp2").attr("src", "images/fp_b1.jpg");
-    $(".fp1").attr("href", "images/fp_b1.jpg");
+    $(".fp2").attr("src", "./images/Products page/accessories1/fp_b1.jpg");
+    $(".fp1").attr("href", "./images/Products page/accessories1/fp_b1.jpg");
 });
 
 $('#btn-white').on('click', function() {
-    $(".fp2").attr("src", "images/fp_w1.jpg");
-    $(".fp1").attr("href", "images/fp_w1.jpg");
+    $(".fp2").attr("src", "./images/Products page/accessories1/fp_w1.jpg");
+    $(".fp1").attr("href", "./images/Products page/accessories1/fp_w1.jpg");
 });

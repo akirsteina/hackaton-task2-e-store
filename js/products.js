37b49fb5eebe41574226797a5336956faf4d6267
Alisenda <78229206+Alisenda@users.noreply.github.com>
2021-05-29 14:06:43 +0300

    update_product2
    add another product type

--- js/products.js
+++ js/products.js
@@ -4,3 +4,29 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
+
+$('#pcwhite').on("click", function(){
+    if ($("#fp1").attr("src","images/fp_b1.jpg")) 
+    {
+        $("#fp1").attr("src","images/fp_b1.jpg")
+    }
+    else 
+    {
+        $("#fp1").attr("src","images/fp_b1.jpg");
+        $("#fp1").attr("href","images/fp_b1.jpg");
+    }
+}
+)
+$('#pcwhite').on("click", function(){
+    if ($("#fp1").attr("src","images/fp_w1.jpg")) 
+    {
+        $("#fp1").attr("src","images/fp_w1.jpg");
+    }
+    else 
+    {
+        $("#fp1").attr("src","images/fp_w1.jpg");
+        $("#fp1").attr("href","images/fp_w1.jpg");
+    }
+}
+)
+

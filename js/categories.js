517e2ac926b132f2a1aead1a5ade15152df59a44
ievabi <ibidermane@gmail.com>
2021-05-30 11:05:44 +0200

    code check, and formatting

--- js/categories.js
+++ js/categories.js
@@ -1,16 +1,16 @@
-$('.btn-indoor').click(function(){
+$('.btn-indoor').click(function () {
     $('.categorie-indoor-list').removeClass('display-off')
     $('.categorie-outdoor-list').addClass('display-off')
     $('.categorie-accessories-list').addClass('display-off')
 })
 
-$('.btn-outdoor').click(function(){
+$('.btn-outdoor').click(function () {
     $('.categorie-outdoor-list').removeClass('display-off')
     $('.categorie-indoor-list').addClass('display-off')
     $('.categorie-accessories-list').addClass('display-off')
 })
 
-$('.btn-accessories').click(function(){
+$('.btn-accessories').click(function () {
     $('.categorie-outdoor-list').addClass('display-off')
     $('.categorie-indoor-list').addClass('display-off')
     $('.categorie-accessories-list').removeClass('display-off')
--- js/products.js
+++ js/products.js
@@ -1,10 +1,10 @@
 // this works, do not change
-$('#btn-black').on('click', function() {
+$('#btn-black').on('click', function () {
     $(".fp2").attr("src", "./images/Products page/accessories1/fp_b1.jpg");
     $(".fp1").attr("href", "./images/Products page/accessories1/fp_b1.jpg");
 });
 
-$('#btn-white').on('click', function() {
+$('#btn-white').on('click', function () {
     $(".fp2").attr("src", "./images/Products page/accessories1/fp_w1.jpg");
     $(".fp1").attr("href", "./images/Products page/accessories1/fp_w1.jpg");
 });
